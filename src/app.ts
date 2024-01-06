da45cae0c0be4a3f1953af576a4f6374c3e81e64
divyuk <div14kes@gmail.com>
2024-01-06 11:57:26 +0530

    app for having middlewares

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import express, { Express, Request, Response, NextFunction } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import userRouter from "./routes/userRoutes";
+// import chatRouter from "./routes/chatRoutes";
+// import messageRouter from "./routes/messageRoutes";
+// import responseMiddleware from "./middleware/responseMiddleware";
+// import AppResponse from "./helpers/AppResponse";
+// import { commonResponseMessages } from "./data/constants";
+
+const app: Express = express();
+
+app.use(cors());
+// set security http headers
+app.use(helmet());
+
+// middleware for parsing request
+app.use(express.json());
+
+// api routes
+app.use("/api/users", userRouter);
+// app.use("/api/chat", chatRouter);
+// app.use("/api/messages", messageRouter);
+
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//   throw next(new AppResponse(commonResponseMessages.RESOURCE_NOT_FOUND));
+// });
+
+// common response middleware
+// app.use(responseMiddleware);
+
+export default app;
