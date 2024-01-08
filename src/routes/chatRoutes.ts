2168a35eea8cc56562bff2d5e2a3d7630fd8512a
divyuk <div14kes@gmail.com>
2024-01-08 15:05:40 +0530

    added validate user chat middleware

--- src/routes/chatRoutes.ts
+++ src/routes/chatRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyToken } from "../controllers/authController";
 import { fetchChatHistory } from "../controllers/chatController";
+import { validateUserChat } from "../middleware/validators";
 
 const router: Router = Router();
 
 router.use(verifyToken);
 
-router.route("/").get(fetchChatHistory).post();
+router.route("/").get(fetchChatHistory).post(validateUserChat);
